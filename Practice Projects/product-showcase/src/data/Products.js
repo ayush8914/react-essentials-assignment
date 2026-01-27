77f321e95c62e60af2cf422a10a007c40ca00ef7
Ayush <ayushgevariya8914@gmail.com>
2026-01-27 23:35:17 +0530

    data: Add Dummy Data

---
+++ Practice Projects/product-showcase/src/data/Products.js
@@ -0,0 +1,39 @@
+  const Products = [
+    {
+      id: 1,
+      name: "Wireless Headphones",
+      description: "High quality wireless headphones.",
+      price: "59.99",
+      image: "/images/headphones.png",
+      isOnSale: false
+    },
+    {
+      id: 2,
+      name: "Smart Watch",
+      description: "Keep track of your fitness and notifications.",
+      price: "99.99",
+      salePrice: "79.99",
+      image: "/images/smartwatch.avif",
+      isOnSale: true
+    },
+    {
+      id: 3,
+      name: "Gaming Laptop",
+      description: "This is a great product.",
+      price: "19.99",
+      salePrice: "14.99",
+      image: "/images/laptop.png",
+      isOnSale: true
+    },
+    {
+      id: 4,
+      name: "iPhone 17 Pro",
+      description: "This product is even better.",
+      price: "29.99",
+      image: "/images/iphone.jpg",
+      isOnSale: false
+    }
+  ]
+
+
+export default Products;
