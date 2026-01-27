3016389fc5cd2bdd68b7ccc412a0c7329005bbfe
Ayush <ayushgevariya8914@gmail.com>
2026-01-27 23:35:49 +0530

    fix: removed redundant code

--- Practice Projects/product-showcase/src/App.js
+++ Practice Projects/product-showcase/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import Section from './components/Section/Section';
+import ProductCard from './components/ProductCard/ProductCard';
+import Products from './data/Products';
 
-function App() {
+
+function App() {  
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="App">
+     <h1>Product Showcase</h1>
+     <Section title="Featured Products">
+       {
+        Products.slice(0,2).map(product => (
+          <ProductCard 
+            key={product.id}
+            image={product.image}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+            isOnSale={product.isOnSale}
+            salePrice={product.salePrice}
+          />
+        ))
+       }
+     </Section>
+
+     <Section title="New Arrivals">
+      {
+        Products.slice(2).map(product => (
+          <ProductCard 
+            key={product.id}
+            {...product}
+          />
+        ))
+      }
+      </Section>
+   </div>
   );
-}
+} 
 
 export default App;
