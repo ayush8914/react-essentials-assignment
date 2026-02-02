e130c2f77e8fb7660cbb5ce615ee685b7e5648dc
Ayush <ayushgevariya8914@gmail.com>
2026-02-02 22:21:09 +0530

    feat: Defined Reducer function and actions

--- Practice Projects/task-manager/src/context/TaskContext.js
+++ Practice Projects/task-manager/src/context/TaskContext.js
@@ -1,4 +1,106 @@
-import { createContext , useContext } from "react";
+import { createContext , useContext , useReducer } from "react";
+
+//Initial state - this is our application's starting point
+const initialState = {
+    tasks:[
+        {
+            id:1,
+            title: "Learn React Context",
+            description : "Understand how context API works",
+            completed : false,
+            priority : "high",
+            createAt :  new Date().toISOString()
+        },
+        {
+            id:2,
+            title: "Learn React Context",
+            description : "Understand how context API works",
+            completed : false,
+            priority : "high",
+            createAt :  new Date().toISOString()
+        },
+        {
+            id:3,
+            title: "Learn React Context",
+            description : "Understand how context API works",
+            completed : false,
+            priority : "high",
+            createAt :  new Date().toISOString()
+        }
+    ],
+    filter : "all", //all ,completed, pending
+    searchTerm:'',
+    isLoading : false,
+    history : [] //for the undo feature
+};
+
+
+// Action types => using contants prevents typos
+export const ACTIONS = {
+    ADD_TASK : 'ADD_TASK',
+    DELETE_TASK : 'DELETE_TASK',
+    TOGGLE_TASK : 'TOGGLE_TASK',
+    EDIT_TASK : 'EDIT_TASK',
+    SET_FILTER : 'SET_FILTER',
+    SET_SEARCH_TERM : 'SET_SEARCH',
+    UNTO_ACTION : 'UNDO_ACTION',
+    SET_LOADING : 'SET_LOADING'
+};
+
+
+//reducer function
+const taskReducer = (state , action) =>{
+    //always save current state to history before making any changes
+
+    const saveToHistory = (currentState) =>({
+        ...currentState,
+        history : [...currentState.history , currentState.history.slice(0,9)] //keep only last 10 state
+    });
+
+    switch(action.type){
+        case ACTIONS.ADD_TASK : 
+            const newTask = {
+                id : Date.now(),
+                title : action.payload.title,
+                description : action.payload.description,
+                completed : false,
+                priority : action.payload.priority,
+                createAt : new Date().toISOString()
+            };
+            return saveToHistory({...state , tasks : [...state.tasks , newTask]});
+        
+        case ACTIONS.DELETE_TASK :
+            return saveToHistory({...state , tasks : state.tasks.filter(task => task.id !== action.payload.id)});
+        
+        case ACTIONS.TOGGLE_TASK :
+            return saveToHistory({...state , tasks : state.tasks.map(task => task.id === action.payload.id ? {...task , completed : !task.completed} : task)}); 
+
+        case ACTIONS.EDIT_TASK :
+            return saveToHistory({...state , tasks : state.tasks.map(task => task.id === action.payload.id ? {...task , ...action.payload.updates} : task)});
+
+        case ACTIONS.SET_FILTER :
+            return saveToHistory({...state , filter : action.payload});
+
+        case ACTIONS.SET_SEARCH_TERM :
+            return saveToHistory({...state , searchTerm : action.payload});
+
+        case ACTIONS.UNTO_ACTION :
+            if(state.history.length >0) 
+            {
+                const [previousState, ...rest] = state.history;
+                return {...previousState , history : rest};
+            }
+
+            return state;
+
+        case ACTIONS.SET_LOADING :
+            return {...state , isLoading : action.payload};
+
+        default :
+            throw new Error(`Unknown action type : ${action.type}`);
+    }
+}
+
 
 
 //step1: create the context;
